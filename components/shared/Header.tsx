f6541f5fa761ac0bf6555dfe1469c62792da6772
Grëg Häris <d.gregharis@gmail.com>
2025-01-22 17:06:30 +0100

    feat: add login and user buttons

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,33 @@
+import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs';
+import { Button } from '@ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="wrapper flex justify-between ">
+        <Link href={'/'} className="w-36">
+          {' '}
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo image"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3 mr-4">
+          <SignedOut>
+            <Button asChild className="rounded-full" size={'lg'}>
+              <SignInButton />
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </header>
+  );
+};
