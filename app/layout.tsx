683bb57251486a9bfefd49ca94552b822d590a30
Grëg Häris <d.gregharis@gmail.com>
2025-01-22 11:37:12 +0100

    feat: import ClerkProvider to handle auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+
+import './globals.css';
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
+  subsets: ['latin'],
+  variable: '--font-poppins',
   weight: ['400', '500', '600', '700'],
 });
 
-
-
 export const metadata: Metadata = {
-  title: "Event",
-  description: "An Event management app",
-  icons: '/assets/images/logo.svg'
+  title: 'Event',
+  description: 'An Event management app',
+  icons: '/assets/images/logo.svg',
 };
 
 export default function RootLayout({
@@ -22,12 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${poppins.variable}antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.variable}antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
