79851f4ca6ca7d04d345ba7e83a51a429a709cfe
Grëg Häris <d.gregharis@gmail.com>
2025-01-23 18:33:13 +0100

    db: add event schemma

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,22 @@
+
+import { Schema, model, models } from 'mongoose';
+
+const EventSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  imageUrl: { type: String, required: true },
+  startDate: { type: Date, required: true, default: Date.now },
+  endDate: { type: Date, required: true, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  price: { type: Number },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Event = models.Event || model('Event', EventSchema);
+
+export default Event;
