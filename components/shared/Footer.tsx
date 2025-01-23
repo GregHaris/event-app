00227d17a392a9af2f871ed3222175462260039b
Grëg Häris <d.gregharis@gmail.com>
2025-01-23 01:41:15 +0100

    feat: add and style the footer items

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex flex-col wrapper flex-between flex-center gap-4 p-5 text-center sm:flex-row">
+        <Link href={'/'}>
+          <Image
+            src={'/assets/images/logo.svg'}
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p className="text-sm"> {currentYear} Evently. All Right Reserved</p>
+      </div>
+    </footer>
+  );
+};
