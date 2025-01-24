d56b291406a07e94c9a4e9c3e07d3a2e3601b9c0
Grëg Häris <d.gregharis@gmail.com>
2025-01-24 20:03:13 +0100

    fix: resolve env variable conflict and remove double id destructuring
    use SIGNING_SECRET and not WEBHOOK_SECRET

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -6,16 +6,18 @@ import { Webhook } from 'svix';
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions';
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
   if (!SIGNING_SECRET) {
     throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
 
-  // Get the headers
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get headers
   const headerPayload = await headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
@@ -23,21 +25,18 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
+    return new Response('Error: Missing Svix headers', {
       status: 400,
     });
   }
 
-  // Get the body
+  // Get body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(SIGNING_SECRET);
-
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
+  // Verify payload with headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
@@ -45,13 +44,13 @@ export async function POST(req: Request) {
       'svix-signature': svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
+    console.error('Error: Could not verify webhook:', err);
+    return new Response('Error: Verification error', {
       status: 400,
     });
   }
 
-  // Get the ID and type
+  // Do something with payload
   const eventType = evt.type;
 
   if (eventType === 'user.created') {
@@ -104,5 +103,5 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'OK', user: deletedUser });
   }
 
-  return new Response('', { status: 200 });
+  return new Response('Webhook received', { status: 200 });
 }
