5bb524abec92de7d57ac6b43c7f02113f295be25
Grëg Häris <d.gregharis@gmail.com>
2025-01-25 20:59:30 +0100

    modify user.update schema

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -81,12 +81,12 @@ export async function POST(req: Request) {
   }
 
   if (eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+    const { id, image_url, email_addresses, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name!,
-      lastName: last_name!,
-      username: username!,
+      username: username || email_addresses[0].email_address.split('@')[0],
+      firstName: first_name ?? '',
+      lastName: last_name ?? '',
       image: image_url,
     };
 
