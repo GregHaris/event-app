890fc894478dce0be4e2782a8ec25da07a0bddaf
Grëg Häris <d.gregharis@gmail.com>
2025-01-28 11:27:19 +0100

    export ICategory interface

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,6 +1,6 @@
 import { Document, Schema, models, model } from 'mongoose';
 
-interface ICategory extends Document {
+export interface ICategory extends Document {
   _id: string;
   name: string;
 }
