857adc1495e0b958e4aae74df4cbc0cc37eb65ce
Grëg Häris <d.gregharis@gmail.com>
2025-01-29 20:32:38 +0100

    feat: add custom jwt session claims type and implement event creation with user id

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -4,18 +4,27 @@ import EventForm from '@shared/EventForm';
 const CreateEvent = async () => {
   const { sessionClaims } = await auth();
 
-  const userId = sessionClaims?.userId as string;
-  
+  // Debugging: Log the entire sessionClaims object
+  console.log('Session Claims:', sessionClaims);
+
+  // Type assertion to help TypeScript understand the structure
+  const claims = sessionClaims as CustomJwtSessionClaims;
+
+  // Access userId from the nested object
+  const userId = claims?.userid?.userId as string;
+
+  console.log('User ID:', userId);
+
   return (
     <>
       {' '}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">
-        Create Event
+          Create Event
         </h3>
       </section>
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create"/>
+        <EventForm userId={userId} type="Create" />
       </div>
     </>
   );
