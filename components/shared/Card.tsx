dd109ceabf2a1c7bd31b815d025e79d8144b2af1
Grëg Häris <d.gregharis@gmail.com>
2025-01-30 18:21:56 +0100

    fix: correct update event path

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -34,7 +34,7 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
       {/* {IS EVENT CREATOR ...} */}
       {isEventCreator && !hidePrice && (
         <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
-          <Link href={`/event/${event._id}/update`}>
+          <Link href={`/events/${event._id}/update`}>
             <Image src={'assets/icons/edit.svg'} alt='edit' width={20} height={20} />
           </Link>
           <DeleteConfirmation eventId={event._id}/>
