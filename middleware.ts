fdfad0cc19c230f308eaacdc6922a25cd4339a7f
Grëg Häris <d.gregharis@gmail.com>
2025-01-31 18:29:54 +0100

    fix: correct events path in public routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
-  '/event/:id',
+  '/events/:id', 
   '/api/webhooks',
   '/api/webhooks/(.*)',
   '/api/uploadthing',
