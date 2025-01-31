dbbb9ce472182d517c3f1bda4eb8e4a0ea812cf3
Grëg Häris <d.gregharis@gmail.com>
2025-01-31 14:25:39 +0100

    fix: correct the url to the event page

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -27,7 +27,7 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
-        href={`/event/${event._id}`}
+        href={`/events/${event._id}`}
         style={{ backgroundImage: `url(${event.imageUrl})` }}
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-gray-500"
       />
@@ -41,7 +41,7 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       )}
       <Link
-        href={`/event/${event._id}`}
+        href={`/events/${event._id}`}
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
       >
         {!hidePrice && (
