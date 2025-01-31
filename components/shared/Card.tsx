55f5753f61c541fef2920dd561cc268262f45087
Grëg Häris <d.gregharis@gmail.com>
2025-01-31 14:30:14 +0100

    fix: format using prettier

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { formatDateTime } from '@/lib/utils';
 import { IEvent } from '@/lib/database/models/event.model';
 
-import {DeleteConfirmation} from './DeleteConfirmation';
+import { DeleteConfirmation } from './DeleteConfirmation';
 
 type CardProps = {
   event: IEvent;
@@ -35,9 +35,14 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
       {isEventCreator && !hidePrice && (
         <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
           <Link href={`/events/${event._id}/update`}>
-            <Image src={'assets/icons/edit.svg'} alt='edit' width={20} height={20} />
+            <Image
+              src={'assets/icons/edit.svg'}
+              alt="edit"
+              width={20}
+              height={20}
+            />
           </Link>
-          <DeleteConfirmation eventId={event._id}/>
+          <DeleteConfirmation eventId={event._id} />
         </div>
       )}
       <Link
